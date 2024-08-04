836decede46992e6cab640512d08ae665eaaf56b
ShakirKadirov <shakirkadirov1@gmail.com>
2024-08-04 11:19:00 +0500

    Initial Commit

---
+++ AstanaWeather/AstanaWeatherApp.swift
@@ -0,0 +1,17 @@
+//
+//  AstanaWeatherApp.swift
+//  AstanaWeather
+//
+//  Created by Shakir Kadirov on 04.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct AstanaWeatherApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
